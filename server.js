b3ba44912b79b7016944b6b07439d3c8c29a907a
Harsh <shry.harsh@gmail.com>
2018-04-14 05:07:16 +0530

    added node and reorganised

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var compression = require('compression');
+
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var page_hit;
+
+
+
+// Start server and enabling comression and cache
+var cacheTime = 86400000;
+
+app.use(compression());
+
+app.use(express.static(__dirname + '/public/', { extensions: ['html', 'htm'], maxAge : cacheTime }));
+
+app.use(bodyParser.urlencoded());
+
+app.listen(80, function(){
+	console.log('Listening on 80');
+/*	fs.readFile(__dirname+'/page_visit', {encoding: 'utf-8'}, function(err, data){
+		if(err){
+			page_hit = 0;
+		}else{
+			page_hit = parseInt(data,10);
+		}
+	});
+*/
+});
